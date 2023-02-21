cb02e92f040943cd90e74b7a99fcd6b62bd6c8fd
Ubuntu <ubuntu@ip-172-31-52-219.ec2.internal>
2023-02-21 20:49:01 +0000

    Subir2

---
+++ funciones.py
@@ -0,0 +1,11 @@
+def sumar(x, y):
+    return x + y
+    
+
+def es_primo(x):
+    if x <= 1:
+        return False
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
